0dd4a1426ceedd28f6081d98f15e034790f53679
wahyuworlder <m.ardiansyah@worldergroup.com>
2025-03-26 02:04:08 +0700

    tambahan fitur 4

--- src/components/Fitur4.jsx
+++ src/components/Fitur4.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Fitur4 = () => {
   return (
-    <h1>Fitur4</h1>
+    <>
+      <h1>Fitur4</h1>
+      <p>
+        tambahan fitur 4
+      </p>
+    </>
   )
 }
 
