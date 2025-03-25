484af57a1536aa5cbd8d635a35cbd35435f89f46
wahyuworlder <m.ardiansyah@worldergroup.com>
2025-03-26 01:59:12 +0700

    tambah fitur 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Fitur1 from "./components/Fitur1"
 import Fitur2 from "./components/Fitur2"
 import Fitur3 from "./components/Fitur3"
+import Fitur4 from "./components/Fitur4"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Fitur1 />
       <Fitur2 />
       <Fitur3 />
+      <Fitur4 />
     </>
   )
 }
---
+++ src/components/Fitur4.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fitur4 = () => {
+  return (
+    <h1>Fitur4</h1>
+  )
+}
+
+export default Fitur4
