1ccdd5e6bc6ca796815ffc23cda12bbd673a42a5
wahyuworlder <m.ardiansyah@worldergroup.com>
2025-03-26 02:00:28 +0700

    tambah fitur 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Fitur1 from "./components/Fitur1"
 import Fitur2 from "./components/Fitur2"
 import Fitur3 from "./components/Fitur3"
 import Fitur4 from "./components/Fitur4"
+import Fitur5 from "./components/Fitur5"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Fitur2 />
       <Fitur3 />
       <Fitur4 />
+      <Fitur5 />
     </>
   )
 }
