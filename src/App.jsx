9393795799d2620f0a1597da5bb55fc740cdcc3a
wahyuworlder <m.ardiansyah@worldergroup.com>
2025-03-26 01:43:26 +0700

    menambahkan fitur 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Fitur1 from "./components/Fitur1"
+import Fitur2 from "./components/Fitur2"
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
       <Fitur1 />
+      <Fitur2/>
     </>
   )
 }
